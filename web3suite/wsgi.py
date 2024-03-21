5e2cba2f57eb84f9cb47243255855b9ed7fa9f58
Elvis <elvis@Elviss-MacBook-Air.local>
2024-03-21 18:34:28 +0530

    changes

--- web3suite/wsgi.py
+++ web3suite/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web3suite.settings')
 
 application = get_wsgi_application()
+
+
+app = application
