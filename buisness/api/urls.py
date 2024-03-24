f3d31b739842217dfff16bbe28528a0af50c2dd9
Elvis <elvis@Elviss-MacBook-Air.local>
2024-03-25 00:34:05 +0530

    changes

--- buisness/api/urls.py
+++ buisness/api/urls.py
@@ -5,8 +5,8 @@ from buisness.api.views import *
 urlpatterns = [
     path('buisness/create', BuisnessCreateAPIView.as_view(), name='buisness-create'),
     path('buisness/<int:pk>', BuisnessRUDView.as_view(), name='buisness-rud'),
-    path('buisness/goals/create', BuisnessGoalsCreateAPIView.as_view(), name='buisness-goals-create'),
-    path('buisness/goals/<int:pk>', BuisnessGoalsRUDView.as_view(), name='buisness-goals-rud'),
+    path('<str:slug>/goals/create', BuisnessGoalsCreateAPIView.as_view(), name='buisness-goals-create'),
+    path('<str:slug>/goals/<int:pk>', BuisnessGoalsRUDView.as_view(), name='buisness-goals-rud'),
     path('<str:slug>/events/create', EventCreateAPIView.as_view(), name='event-create'),
     path('<str:slug>/events/<int:pk>', EventRUDView.as_view(), name='event-rud'),
     path('<str:slug>/events/list', EventListAPIView.as_view(), name='event-list'),
