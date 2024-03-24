bf4dfeeefe0eedf66ae7b03ab1046f28194dea0d
Elvis <elvis@Elviss-MacBook-Air.local>
2024-03-24 23:51:44 +0530

    changes

--- buisness/api/serializers.py
+++ buisness/api/serializers.py
@@ -4,6 +4,7 @@ from buisness.models import *
 
 class BuisnessSerializer(serializers.ModelSerializer):
     slug = serializers.StringRelatedField(read_only=True)
+    user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Businesses
         fields = ['id', 'user', 'name', 'crn', 'industry', 'address',
