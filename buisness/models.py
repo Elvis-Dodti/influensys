61606537ded8e65e54ed12338abb295cc34570ab
Elvis <elvis@Elviss-MacBook-Air.local>
2024-03-25 00:00:50 +0530

    changes

--- buisness/models.py
+++ buisness/models.py
@@ -16,6 +16,7 @@ class Businesses(models.Model):
     facebook = models.CharField(max_length=255, blank=True, null=True)
     instagram = models.CharField(max_length=255, blank=True, null=True)
     website = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
     # image = models.ImageField(upload_to='media/', blank=True, null=True)
     slug = models.CharField(max_length=100, unique=True)
 
