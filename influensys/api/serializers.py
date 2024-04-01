142dcae6ce000d4cb0e1cf5ca53d2620d33717fc
elvis <elvis.mystiqlabs@gmail.com>
2024-04-01 21:49:32 +0530

    changes

--- influensys/api/serializers.py
+++ influensys/api/serializers.py
@@ -3,6 +3,24 @@ from influensys.models import *
 
 
 class InfluencerSerializer(serializers.ModelSerializer):
+    slug = serializers.StringRelatedField(read_only=True)
+    user = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Influencers
-        fields = '__all__'
+        fields = [
+            "user",
+            "name",
+            "description",
+            "email",
+            "phone",
+            "industry",
+            "address",
+            "country",
+            "pincode",
+            "annual_revenue",
+            "facebook",
+            "instagram",
+            "website",
+            "slug"
+        ]
